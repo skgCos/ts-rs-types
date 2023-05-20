c5998d17bbac579285dc7059b7e782e1800b4055
skgCos <51274191+skgCos@users.noreply.github.com>
2023-05-19 23:23:17 -0700

    Add more tests to test flatten enum

--- ts-rs/tests/flatten.rs
+++ ts-rs/tests/flatten.rs
@@ -1,8 +1,9 @@
 #![allow(dead_code)]
 
 use ts_rs::TS;
+use serde::{Deserialize, Serialize};
 
-#[derive(TS)]
+#[derive(Serialize, Deserialize, TS)]
 struct A {
     a: i32,
     b: i32,
@@ -27,5 +28,115 @@ fn test_def() {
     assert_eq!(
         C::inline(),
         "{ b: ({ a: number } & { b: number }) & { c: number } } & { d: number }"
+    );
+}
+
+#[derive(Serialize, Deserialize, TS)]
+struct D {
+    #[serde(flatten)]
+    b: E,
+    d: i32,
+}
+
+#[derive(Serialize, Deserialize, TS)]
+#[serde(tag = "t", content = "c")]
+enum E {
+    Something1(A),
+    Something2(A)
+}
+
+#[test]
+fn test_flatten_adjacently_unnamed() {
+    assert_eq!(
+        D::inline(),
+        "({ t: \"Something1\", c: A } | { t: \"Something2\", c: A }) & { d: number }"
+    );
+}
+
+#[derive(Serialize, Deserialize, TS)]
+struct F {
+    #[serde(flatten)]
+    b: G,
+    d: i32,
+}
+
+#[derive(Serialize, Deserialize, TS)]
+#[serde(tag = "t", content = "c")]
+enum G {
+    Something1,
+    Something2
+}
+
+#[test]
+fn test_flatten_adjacently_unit() {
+    assert_eq!(
+        F::inline(),
+        "({ t: \"Something1\" } | { t: \"Something2\" }) & { d: number }"
+    );
+}
+
+#[derive(Serialize, Deserialize, TS)]
+struct H {
+    #[serde(flatten)]
+    b: I,
+    d: i32,
+}
+
+#[derive(Serialize, Deserialize, TS)]
+#[serde(tag = "t", content = "c")]
+enum I {
+    Something1(),
+    Something2()
+}
+
+#[test]
+fn test_flatten_adjacently_none() {
+    assert_eq!(
+        H::inline(),
+        "({ t: \"Something1\", c: null } | { t: \"Something2\", c: null }) & { d: number }"
+    );
+}
+
+#[derive(Serialize, Deserialize, TS)]
+struct J {
+    #[serde(flatten)]
+    b: K,
+    d: i32,
+}
+
+#[derive(Serialize, Deserialize, TS)]
+#[serde(tag = "t")]
+enum K {
+    Something1(A),
+    Something2(A)
+}
+
+#[test]
+fn test_flatten_internally_none_unnamed() {
+    assert_eq!(
+        J::inline(),
+        "({ t: \"Something1\" } & A | { t: \"Something2\" } & A) & { d: number }"
+    );
+}
+
+#[derive(Serialize, Deserialize, TS)]
+struct L {
+    #[serde(flatten)]
+    b: M,
+    d: i32,
+}
+
+#[derive(Serialize, Deserialize, TS)]
+#[serde(tag = "t")]
+enum M {
+    Something1,
+    Something2
+}
+
+#[test]
+fn test_flatten_internally_none_unit() {
+    assert_eq!(
+        J::inline(),
+        "({ t: \"Something1\" } & A | { t: \"Something2\" } & A) & { d: number }"
     );
 }
